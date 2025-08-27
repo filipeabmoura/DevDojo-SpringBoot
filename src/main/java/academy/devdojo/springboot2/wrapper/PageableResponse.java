6ef55d69adaa69cf3a1a3b0341d6b8d4a33d7506
filipeabmoura <filipemouradev@gmail.com>
2025-08-27 14:45:09 -0300

    test e config: teste de integração do Controller e criação de Profile integration-tests

--- src/main/java/academy/devdojo/springboot2/wrapper/PageableResponse.java
+++ src/main/java/academy/devdojo/springboot2/wrapper/PageableResponse.java
@@ -2,7 +2,6 @@ package academy.devdojo.springboot2.wrapper;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
